bb6b99be48980a97b5d509b216ce0609ab9ee3e4
premsomashekar <premakashkumar20@gmail.com>
2022-11-07 08:57:52 +0000

    GIT Comment

--- webapp/controller/material.controller.js
+++ webapp/controller/material.controller.js
@@ -1,21 +1,43 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("mm.materialmanagement.controller.material", {
             onInit: function () {
-                debugger;
-                // var oTable = this.getView().byId("table");
-                // oTable.bindElement("/MatBatchSet", {
-                //     path: 'MaterialId',
-                //     descending: false
-                // });
+                this.materialItem = new JSONModel();
 
+            },
+            createMaterial: function () {
+                var that = this;
+                if (!that._Dialog) {
+                    // Load fragment
+                    that._Dialog = sap.ui.core.Fragment.load({
+                        id: that.getView().getId(),
+                        name: "mm.materialmanagement.fragments.CreateMaterial",
+                        controller: that
+                    }).then(function (oDialog) {
+                        that.getView().addDependent(oDialog);
+                        return oDialog;
+                    }.bind(that));
+                }
+
+                that._Dialog.then(function (oDialog) {
+                    // Open dialog and set the model
+                    that.oDialog = oDialog;
+                    oDialog.open();
+                });
+
+            },
+            onCancel: function () {
+                //On press of cancel button close the dialog
+                this.oDialog.close();
             }
+
         });
     });
