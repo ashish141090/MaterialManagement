4d70c20b6f745e833390285055e483a16c2c3fdd
premsomashekar <premakashkumar20@gmail.com>
2022-11-08 06:21:04 +0000

    GIT commit

--- webapp/controller/material.controller.js
+++ webapp/controller/material.controller.js
@@ -10,15 +10,31 @@ sap.ui.define([
 
         return Controller.extend("mm.materialmanagement.controller.material", {
             onInit: function () {
-                this.materialItem = new JSONModel();
-
+                this.oMaterial = new JSONModel();
+                this.oMaterial.setDefaultBindingMode(sap.ui.model.BindingMode.TwoWay);
+                this.oMaterialJson = {};
+                this.oMaterialJson.MaterialId = "";
+                this.oMaterialJson.Batch = "";
+                this.oMaterialJson.ProducedOn = "";
+                this.oMaterialJson.Plant = "";
             },
-            createMaterial: function () {
+
+            clearMaterial: function () {
+                this.oMaterialJson.MaterialId = "";
+                this.oMaterialJson.Batch = "";
+                this.oMaterialJson.ProducedOn = "";
+                this.oMaterialJson.Plant = "";
+            },
+
+            createMaterial: function () {                
+                this.clearMaterial();
+                this.oMaterial.setData(this.oMaterialJson);
                 var that = this;
-                if (!that._Dialog) {
+                
+                if (!that._CreateDialog) {
                     // Load fragment
-                    that._Dialog = sap.ui.core.Fragment.load({
-                        id: that.getView().getId(),
+                    that._CreateDialog = sap.ui.core.Fragment.load({
+                        id: that.getView().getId() + "Create",
                         name: "mm.materialmanagement.fragments.CreateMaterial",
                         controller: that
                     }).then(function (oDialog) {
@@ -27,16 +43,64 @@ sap.ui.define([
                     }.bind(that));
                 }
 
-                that._Dialog.then(function (oDialog) {
+                that._CreateDialog.then(function (oCreateDialog) {
                     // Open dialog and set the model
-                    that.oDialog = oDialog;
-                    oDialog.open();
+                    that.oCreateDialog = oCreateDialog;
+                    oCreateDialog.setModel(that.oMaterial);
+                    oCreateDialog.open();
                 });
 
             },
-            onCancel: function () {
+
+            onCreateCancel: function () {
                 //On press of cancel button close the dialog
-                this.oDialog.close();
+                this.oCreateDialog.close();
+            },
+
+            onCreate: function () {
+                //On press of cancel button close the dialog
+                this.oCreateDialog.close();
+            },
+
+            onChangeCancel: function () {
+                //On press of cancel button close the dialog
+                this.oChangeDialog.close();
+            },
+
+            onChange: function () {
+                //On press of cancel button close the dialog
+                this.oChangeDialog.close();                
+            },
+
+            changeMaterial: function () {     
+                var sSelectedMat = this.getView().byId("table").getSelectedContexts()[0].getProperty();
+                this.oMaterialJson.MaterialId = sSelectedMat.MaterialId;
+                this.oMaterialJson.Batch = sSelectedMat.Batch;
+                this.oMaterialJson.ProducedOn = sSelectedMat.ProducedOn;
+                this.oMaterialJson.Plant = sSelectedMat.Plant;
+                var that = this;
+
+                this.oMaterial.setData(this.oMaterialJson);
+
+                if (!that._ChangeDialog) {
+                    // Load fragment
+                    that._ChangeDialog = sap.ui.core.Fragment.load({
+                        id: that.getView().getId() + "Change",
+                        name: "mm.materialmanagement.fragments.ChangeMaterial",
+                        controller: that
+                    }).then(function (oChangeDialog) {
+                        that.getView().addDependent(oChangeDialog);
+                        return oChangeDialog;
+                    }.bind(that));
+                }
+
+                that._ChangeDialog.then(function (oChangeDialog) {
+                    // Open dialog and set the model
+                    that.oChangeDialog = oChangeDialog;
+                    oChangeDialog.setModel(that.oMaterial);
+                    oChangeDialog.open();
+                });
+
             }
 
         });
